0b3ed02b2e6c2eff1f2beb404cf7aed76b6d6f0d
Fernando Alencar Jr <loidiol@hotmail.com>
2020-04-17 08:06:50 -0300

    cascata ok 0/5

--- Dialogos/dialogoUsuario.js
+++ Dialogos/dialogoUsuario.js
@@ -3,7 +3,6 @@ const { channels } = require('botbuilder-dialogs/lib/choices/channel');
 const {AttachmentPrompt,ChoiceFactory,ChoicePrompt,ComponentDialog,ConfirmPrompt,DialogSet,DialogTurnStatus,NumberPrompt,TextPrompt,WaterfallDialog } = require('botbuilder-dialogs')
 const { perfilUsuario } = require('../perfilUsuario')
 
-
 const  ATTACHMENT_PROMPT  =  'ATTACHMENT_PROMPT' 
 const  CHOICE_PROMPT  =  'CHOICE_PROMPT' 
 const  CONFIRM_PROMPT  =  'CONFIRM_PROMPT' 
@@ -12,7 +11,7 @@ const  NUMBER_PROMPT  =  'NUMBER_PROMPT'
 const  USER_PROFILE  =  'USER_PROFILE' 
 const  WATERFALL_DIALOG  =  'WATERFALL_DIALOG' 
 
-class dialogoUsuario extends ComponentDialog{
+class dialogoUsuario extends ComponentDialog {
   constructor(userState) {
     super('dialogoUsuario')
     
@@ -20,18 +19,18 @@ class dialogoUsuario extends ComponentDialog{
     this.addDialog(new TextPrompt(NAME_PROMPT))
     this.addDialog(new ChoicePrompt(CHOICE_PROMPT))
     this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
-    this.addDialog(new NumberPrompt(NUMBER_PROMPT,this.idadePromptValidador))
+    this.addDialog(new NumberPrompt(NUMBER_PROMPT, this.cpfPromptValidador))
     this.addDialog(new AttachmentPrompt(ATTACHMENT_PROMPT))
     this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
       this.inicioStep.bind(this),
       this.nomeStep.bind(this),
       this.nomeConfirmStep.bind(this),
-     /* this.cpfStep.bind(this),
-      this.cpfConfirmStep.bind(this),
       this.idadeStep.bind(this),
-      this.pictureStep.bind(this),
-      this.confirmStep.bind(this),
-      this.summaryStep.bind(this)*/
+      /* this.cpfStep.bind(this),
+       this.cpfConfirmStep.bind(this),
+       this.pictureStep.bind(this),
+       this.confirmStep.bind(this),
+       this.summaryStep.bind(this)*/
 
 
     ]))
@@ -48,20 +47,35 @@ class dialogoUsuario extends ComponentDialog{
       await contextoDialogo.beginDialog(this.id)
     }
   }
+
+  /// aki e o começo da cascata
+  
   async inicioStep(step) {
     return await step.prompt(CHOICE_PROMPT, {
       prompt: 'Olá bem vindo, esta é a sua primeira visita ?',
-      choices: ChoiceFactory.toChoices(['Sim','Não'])
-  })
-}
+      choices: ChoiceFactory.toChoices(['Sim', 'Não'])
+    })
+  }
   async nomeStep(step) {
     step.values.inicio = step.result.value
-    return await step.prompt(NAME_PROMPT,'Porfavor digite o seu nome')
+    return await step.prompt(NAME_PROMPT, 'Porfavor digite o seu nome')
   }
   async nomeConfirmStep(step) {
-    step.values.name = step.result
+    step.values.nome = step.result
     await step.context.sendActivity(`Obrigado ${step.result} .`)
-    return await step.prompt(CONFIRM_PROMPT,'Quer falar a sua idade ?',['Sim','Não'] )
-}
+    return await step.prompt(CONFIRM_PROMPT, 'Voce possui maior de 18 anos ?', ['Sim', 'Não'])
+  }
+  async idadeStep(step) {
+    step.values.idade = step.result
+    const checkIdade = step.result
+    console.log(checkIdade)
+    if (checkIdade === true) {
+     await step.context.sendActivity('tudo certo!') 
+    } else {
+      await step.context.sendActivity('Somente maiores de idade podera permanecer neste site')
+    }
+    return await step.context.sendActivity('cascata tudo certo ate aki')
+  }
+
 }
 module.exports.dialogoUsuario = dialogoUsuario
--- bot.js
+++ bot.js
@@ -7,15 +7,13 @@ const { LuisRecognizer } = require('botbuilder-ai')
 class EmptyBot extends ActivityHandler {
     constructor(conversationState, userState, dialog) {
       super();
-      if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
-      if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
-      if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
+
       this.conversationState = conversationState;
       this.userState = userState;
       this.dialog = dialog;
       this.dialogState = this.conversationState.createProperty('DialogState');
       this.onMessage(async (context, next) => {
-        console.log('Running dialog with Message Activity.');
+        console.log('executando o diálogo');
 
         await this.dialog.run(context, this.dialogState);
 
@@ -37,7 +35,7 @@ class EmptyBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Bem Vindo !')
+                    await context.sendActivity('Bem Vindo!')
                 }
             }
             await next()
@@ -45,7 +43,6 @@ class EmptyBot extends ActivityHandler {
   }
   async run(context) {
     await super.run(context);
-
     await this.conversationState.saveChanges(context, false);
     await this.userState.saveChanges(context, false);
 }
